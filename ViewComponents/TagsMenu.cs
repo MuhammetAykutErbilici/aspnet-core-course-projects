abcceea25f7c16f872ef9c6b31e6000713f07492
Muhammet Aykut Erbilici <ajnd114@gmail.com>
2026-06-05 19:37:05 +0300

    Add files via upload

---
+++ ViewComponents/TagsMenu.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq; 
+using BlogApp.Web.Data.Abstract; 
+using Microsoft.EntityFrameworkCore; 
+
+namespace BlogApp.Web.ViewComponents 
+{
+    public class TagsMenu : ViewComponent
+    {
+        private readonly ITagRepository _tagRepository; 
+
+        public TagsMenu(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _tagRepository.Tags.ToListAsync());
+        }
+    }
+}
